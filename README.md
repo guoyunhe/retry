3449ab22efde7f82f8c4c0bc204e7a3db72f31f6
Guo Yunhe <i@guoyunhe.me>
2024-12-28 12:33:00 +0800

    first version

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# retry
+# @guoyunhe/retry
+
 a better retry function
+
+## Installation
+
+```bash
+npm install --save @guoyunhe/retry
+```
+
+## Examples
+
+```js
+import { retry } from '@guoyunhe/retry';
+
+// By default, it will retry max. 5 times on any error.
+await retry(() => fetch('/foobar.json'));
+
+// You can define a different max. retry limit, even Infinity.
+await retry(() => fetch('/foobar.json'), { retries: 666 });
+
+// You can also control whether to continue retrying by detect error type or message.
+await retry(() => fetch('/foobar.json'), {
+  shouldRetry: (e) => e.name === 'TypeError' && e.message === 'Failed to fetch',
+});
+
+// Retrying too often can crash systems. Use retryDelay to reduce retry frequence.
+await retry(() => fetch('/foobar.json'), {
+  retryDelay: (retryIndex) => 1000 * Math.pow(retryIndex, 2);
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
 export interface RetryOptions {
-  lastNameUpperCase?: boolean;
+  /**
+   * How many times to retry.
+   * @default 5
+   */
+  retries?: number;
+  /**
+   * Decide to retry or not based on catched error.
+   * @default () => true
+   */
+  shouldRetry?: (e: Error) => boolean;
+  /**
+   * Adding delay (in milliseconds) to avoid retrying too often and crashing the system.
+   * @default 0
+   */
+  retryDelay?: number | ((nextRetryCount: number) => number);
 }
 
-export function retry(firstName: string, lastName: string, options?: RetryOptions) {
-  if (options?.lastNameUpperCase) {
-    return firstName + ' ' + lastName.toLocaleUpperCase();
+export async function retry<T>(
+  run: () => Promise<T>,
+  { retries = 5, shouldRetry = () => true, retryDelay = 0 }: RetryOptions = {},
+): Promise<T> {
+  for (let i = 0; i <= retries; i++) {
+    if (i > 0 && retryDelay) {
+      await sleep(typeof retryDelay === 'function' ? retryDelay(i) : retryDelay);
+    }
+    try {
+      return await run();
+    } catch (e) {
+      if (shouldRetry(e)) {
+        continue;
+      } else {
+        throw e;
+      }
+    }
   }
-  return firstName + ' ' + lastName;
+}
+
+function sleep(ms: number) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, ms);
+  });
 }
